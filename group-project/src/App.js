d4523d5b6451b055c7733c4b04358a73acef5531
Lucy <lucysargent@y7mail.com>
2021-11-20 11:06:51 +1000

    render data on homepage

--- group-project/src/App.js
+++ group-project/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import "./App.css";
 
 function App() {
+  const [profileData, setProfileData] = useState([]);
+
+  useEffect(() => {
+    console.log("Hi");
+    fetch(`${process.env.REACT_APP_API_URL}projects/`)
+      .then((results) => {
+        return results.json();
+      })
+      .then((data) => {
+        setProfileData(data);
+        console.log("data: ", data);
+      })
+      .catch((e) => {
+        console.log("OH NOOO: ", e);
+      });
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Tech is Me.</h1>
+        <Router>
+          <Switch>
+            <Route path="/projects/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
