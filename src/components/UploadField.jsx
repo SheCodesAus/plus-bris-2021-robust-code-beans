68ce0d572674057daf58b88bfd80c9adbf3a73a4
sghavanloo <86653446+sghavanloo@users.noreply.github.com>
2021-11-23 19:59:23 +1000

    remove comments

--- src/components/UploadField.jsx
+++ src/components/UploadField.jsx
@@ -1,40 +1,28 @@
 import React, {Component} from "react";
 import axios from "axios"
-// import './App.css';
 
-// function Upload () {
-//     return (
-//         <div class="upload-component">
-//             <h1> this is test </h1>
-//         </div>
-//       );
-// }
-// export default Upload
 class UploadField extends Component {
     state = {
         selectedFile:null
     }
-    fileSelectedHandler = event => {
-        // console.log(event.target.files[0]);
-        
+
+    fileSelectedHandler = event => {        
         this.setState({
             selectedFile: event.target.files[0]
         })
     }
+
     fileUploadHandler=() => {
         console.log('api url:', REACT_APP_API_URL)
         const fd = new FormData();
         fd.append('image', this.state.selectedFile, this.state.selectedFile.name);
-        axios.post(REACT_APP_API_URL, fd)
-        //formdata 
-           
+        axios.post(REACT_APP_API_URL, fd)  
         }
-        
+
     render() {
         return (
             <div className="App">
-              <input type="file" onChange={this.fileSelectedHandler}/> 
-              {/* <button onClick={}> Select Photo </button> */}
+              <input type="file" onChange={this.fileSelectedHandler}/>
               <button onClick={this.fileUploadHandler}>Upload</button>         
             </div>
         );
