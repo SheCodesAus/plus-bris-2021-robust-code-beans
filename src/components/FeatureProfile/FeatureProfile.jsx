813b35bf5185804128d13d1827cb0f09a3e13295
Lucy <lucysargent@y7mail.com>
2021-11-28 13:40:03 +1000

    resolve merge conflict

---
+++ src/components/FeatureProfile/FeatureProfile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const feature_profile = (props) => {
+    if (filtered.id === 1)
+    console.log("found profile1")
+    return (
+      <div className="Feature-Profile"> 
+        <ProfileCard />
+      </div>
+    )
+  }
+
+
