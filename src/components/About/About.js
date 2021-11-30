eb66d80ab4a85eafd4c9fda685e7a4ce7307444e
JMck3 <themckeerings@gmail.com>
2021-11-30 11:51:06 +1000

    added shimmer background to container

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,12 @@
 import React from "react";
+import "./About.css";
 
 function About() {
   return (
     <div className="about">
       <div class="container">
-        background: #6495ed;
         <img
-          class="img-fluid rounded mb-4 mb-lg-0"
+          class="img-fluid"
           src="https://media.istockphoto.com/photos/female-startup-business-team-picture-id1039076422?b=1&k=20&m=1039076422&s=170667a&w=0&h=_2QSPKDWmPmw3a1VxjxcHSpoNaYyygh6aRvms9C_X2U="
           alt=""
         />
@@ -27,11 +27,14 @@ function About() {
           At She Codes we are trying to change that, this website was created as
           a hub where the community can create profiles of women and non-binary
           folk. The idea would be that we could share this website to show young
-          people that anyone can work in Tech. This project outline above formed
-          the basis of our final group project for She Codes Brisbane Cohort
-          2021. Our group, the RobustCodingBeans, worked with our client Sara
-          Borgstrom (BHP) to develop a MVP (Minimum Viable Product) website with
-          the product aim above.
+          people that anyone can work in Tech.
+          <br></br>
+          <br></br>
+          This website is our our final group project for She Codes Brisbane
+          Cohort 2021 and an example of women giving "Tech" a go! Our group, the
+          RobustCodingBeans, worked with our client Sara Borgstrom (BHP) to
+          develop a MVP (Minimum Viable Product) website with the product aim
+          above.
           <br></br>
           <br></br>
           An MVP is the base features and functionality required by the client
