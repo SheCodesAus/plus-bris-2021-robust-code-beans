0134b9d206dc6d0ce56ebc1d45126d723a774b97
Lucy <lucysargent@y7mail.com>
2021-12-01 22:00:20 +1000

    getting profiles back

--- src/components/Profile/ProfileCard.jsx
+++ src/components/Profile/ProfileCard.jsx
@@ -5,26 +5,24 @@ const ProfileCard = ({ profile, featured }) => {
   const formattedDate = new Date(profile.date_created).toLocaleDateString();
 
   return (
-    
     <div className={featured ? "featured-profile-card" : "profile-card"}>
       <div className="feature-column-a">
         {/* <p>{profile.id}</p> */}
-      {featured && (
-        <p className="featured-profile-title">Meet {profile.first_name}</p>
-      )}
+        {featured && (
+          <p className="featured-profile-title">Meet {profile.first_name}</p>
+        )}
         <img className="profile-photo" src={profile.photo} />
+      </div>
+      <div className="feature-column-b">
         <h2>{profile.first_name}</h2>
         <p>Gender: {profile.gender}</p>
         <p>Company: {profile.company}</p>
         <p>Role: {profile.role}</p>
         <p>Experience: {profile.experience} years</p>
-      </div>
-      <div>
-        <p>{profile.bio}</p>
-        <p>Facts! {profile.facts}</p>
         <p>
           <a href={profile.linkedin}>
-            <img className="linkedin-icon"
+            <img
+              className="linkedin-icon"
               src={
                 "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
               }
@@ -33,6 +31,12 @@ const ProfileCard = ({ profile, featured }) => {
           </a>
         </p>
         <p className="posted-text">Posted: {formattedDate}</p>
+      </div>
+
+      <div className="feature-column-c">
+        <p>{profile.bio}</p>
+        <p>Facts! {profile.facts}</p>
+        
       </div>
       {/* <p>{profile.status}</p> */}
     </div>
